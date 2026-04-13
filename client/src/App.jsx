d83b19b9b8decb035b6720b9211ee97daead470d
26-Shiv-Gupta <gedashiv9977@gmail.com>
2026-04-14 01:41:13 +0530

    react router dom added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,21 @@
 import { useState } from 'react'
 import './App.css'
+import {Routes, Route } from "react-router-dom"
+import Home from './pages/Home'
+import MyBookings from './pages/MyBookings'
+import Cars from './pages/Cars'
+import CarDetails from './pages/CarDetails'
 
 function App() {
 
   return (
     <>
-      <div>Ap</div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cars" element={<Cars />} />
+        <Route path="/my-bookings" element={<MyBookings />} />
+        <Route path="/car-details" element={<CarDetails />} />
+      </Routes>
     </>
   )
 }
